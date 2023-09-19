2d3a5f7bf2195e8138cb0c6bc71747a9334718ed
Asma <asmaa.abualkheir@gmail.com>
2023-09-19 17:33:10 +0200

    Add project files.

---
+++ ConsoleApp2/assignment/utilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ConsoleApp2.assignment
+{
+    public class utilities
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+           String UserName = Console.ReadLine();
+
+            Console.WriteLine(UserName);
+
+            Console.WriteLine("Enter your favorite color: ");
+            String FavoriteColor = Console.ReadLine();
+            Console.WriteLine("#" + FavoriteColor);
+
+            String message = "Please enter your mothertongue: ";
+            Console.WriteLine(message);
+            String mothertongue = Console.ReadLine();
+            Console.WriteLine(mothertongue);
+
+            String word = "season";
+            Console.WriteLine("Please enter a " + word);
+            String text = Console.ReadLine();
+            Console.WriteLine(text);
+
+            String FirstName = "Asmaa";
+            String LastName = "Abualkheir";
+            Console.WriteLine(LastName + ", " + FirstName);
+
+
+            
+        }
+    }
+}
